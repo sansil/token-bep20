6fca9c6e84ccb2ed31537df80a5044244029cdcf
sansil <sansil32@gmail.com>
2021-10-29 14:46:35 -0300

    first commit

---
+++ contracts/Stakable.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+/**
+ * @notice Stakeable is a contract who is ment to be inherited by other contract that wants Staking capabilities
+ */
+contract Stakeable {
+    /**
+     * @notice Constructor since this contract is not ment to be used without inheritance
+     * push once to stakeholders for it to work proplerly
+     */
+    constructor() {
+        // This push is needed so we avoid index 0 causing bug of index-1
+        stakeholders.push();
+    }
+
+    /**
+     * @notice
+     * A stake struct is used to represent the way we store stakes,
+     * A Stake will contain the users address, the amount staked and a timestamp,
+     * Since which is when the stake was made
+     */
+    struct Stake {
+        address user;
+        uint256 amount;
+        uint256 since;
+    }
+    /**
+     * @notice Stakeholder is a staker that has active stakes
+     */
+    struct Stakeholder {
+        address user;
+        Stake[] address_stakes;
+    }
+
+    /**
+     * @notice
+     *   This is a array where we store all Stakes that are performed on the Contract
+     *   The stakes for each address are stored at a certain index, the index can be found using the stakes mapping
+     */
+    Stakeholder[] internal stakeholders;
+    /**
+     * @notice
+     * stakes is used to keep track of the INDEX for the stakers in the stakes array
+     */
+    mapping(address => uint256) internal stakes;
+    /**
+     * @notice Staked event is triggered whenever a user stakes tokens, address is indexed to make it filterable
+     */
+    event Staked(
+        address indexed user,
+        uint256 amount,
+        uint256 index,
+        uint256 timestamp
+    );
+}
